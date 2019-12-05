9a2851703bff9215f87c5d83977626d420fb3b20
liang <402284347@qq.com>
2019-12-05 23:46:07 +0800

    访问Documents 目录，flutter_swiper插件实现轮播图

--- lib/views/write/write-view.dart
+++ lib/views/write/write-view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 import 'dart:async';
 import 'dart:io';
-
-import 'package:image_picker/image_picker.dart';
 
 class WriteView extends StatefulWidget {
   @override
@@ -65,7 +67,7 @@ class _WriteState extends State<WriteView> {
 
   void _onImageButtonPressed(ImageSource source) async {
     try {
-      _imageFile = await ImagePicker.pickImage(source: source);
+      _imageFile = await ImagePicker.pickImage(source: source, maxWidth: 100);
       setState(() {
         _imageFileList.add(_imageFile);
       });
@@ -74,144 +76,190 @@ class _WriteState extends State<WriteView> {
     }
   }
 
+  List<Widget> _buildAddImageChoiceList() {
+    return [
+      GestureDetector(
+        onTap: () {
+          _onImageButtonPressed(ImageSource.gallery);
+          Navigator.pop(context);
+        },
+        child: Container(
+          padding: EdgeInsets.only(top: 10, bottom: 10),
+          child: Text(
+            'photo',
+            textAlign: TextAlign.center,
+            style: new TextStyle(
+              color: Colors.black38,
+              decoration: TextDecoration.none,
+              fontWeight: FontWeight.w400,
+              fontSize: 22.0,
+            ),
+          ),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(16.0),
+                  topRight: Radius.circular(16.0))),
+          width: 300,
+        ),
+      ),
+      Container(
+        decoration: BoxDecoration(
+          color: Colors.grey,
+        ),
+        width: 300,
+        height: 1,
+      ),
+      GestureDetector(
+        onTap: () {
+          _onImageButtonPressed(ImageSource.camera);
+          Navigator.pop(context);
+        },
+        child: Container(
+          padding: EdgeInsets.only(top: 10, bottom: 10),
+          child: Text(
+            'camera',
+            textAlign: TextAlign.center,
+            style: new TextStyle(
+              color: Colors.black38,
+              decoration: TextDecoration.none,
+              fontWeight: FontWeight.w400,
+              fontSize: 22.0,
+            ),
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(16.0),
+                bottomRight: Radius.circular(16.0)),
+            color: Colors.white,
+          ),
+          width: 300,
+        ),
+      ),
+    ];
+  }
+
+  void _previewImage() async {
+    await showDialog(
+      context: context,
+      builder: (ctx) {
+        return Swiper(
+          itemBuilder: (BuildContext context, int index) {
+            return new Image.network(
+              "http://via.placeholder.com/350x150",
+              fit: BoxFit.fill,
+            );
+          },
+          itemCount: 3,
+          index: 1,
+          pagination: new SwiperPagination(),
+        );
+      },
+    );
+  }
+
+  Widget _buildAddImageEvent() {
+    return GestureDetector(
+      onTap: () async {
+        await showDialog(
+          context: context,
+          builder: (ctx) {
+            return Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: _buildAddImageChoiceList(),
+              ),
+            );
+          },
+        );
+      },
+      child: Container(
+        // red box
+        child: Icon(
+          Icons.camera_alt,
+          color: Colors.grey[500],
+        ),
+        decoration: BoxDecoration(
+          color: Colors.grey[300],
+        ),
+        width: 100,
+        height: 120,
+      ),
+    );
+  }
+
   List<Widget> _buildList() {
     var list = <Widget>[
 //      Text(_imageFileList.length.toString()),
-      GestureDetector(
-        onTap: () async {
-          await showDialog(
-            context: context,
-            builder: (ctx) {
-              return Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    GestureDetector(
-                      onTap: () {
-                        _onImageButtonPressed(ImageSource.gallery);
-                        Navigator.pop(context);
-                      },
-                      child: Container(
-                        padding: EdgeInsets.only(top: 10, bottom: 10),
-                        child: Text(
-                          'photo',
-                          textAlign: TextAlign.center,
-                          style: new TextStyle(
-                            color: Colors.black38,
-                            decoration: TextDecoration.none,
-                            fontWeight: FontWeight.w400,
-                            fontSize: 22.0,
-                          ),
-                        ),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(16.0),
-                                topRight: Radius.circular(16.0))),
-                        width: 300,
-                      ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.grey,
-                      ),
-                      width: 300,
-                      height: 1,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        _onImageButtonPressed(ImageSource.camera);
-                        Navigator.pop(context);
-                      },
-                      child: Container(
-                        padding: EdgeInsets.only(top: 10, bottom: 10),
-                        child: Text(
-                          'camera',
-                          textAlign: TextAlign.center,
-                          style: new TextStyle(
-                            color: Colors.black38,
-                            decoration: TextDecoration.none,
-                            fontWeight: FontWeight.w400,
-                            fontSize: 22.0,
-                          ),
-                        ),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(16.0),
-                              bottomRight: Radius.circular(16.0)),
-                          color: Colors.white,
-                        ),
-                        width: 300,
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            },
-          );
-        },
-        child: Container(
-          // red box
-          child: Icon(
-            Icons.camera_alt,
-            color: Colors.grey[500],
-          ),
-          decoration: BoxDecoration(
-            color: Colors.grey[300],
-          ),
-          width: 100,
-          height: 120,
-        ),
-      ),
+      _buildAddImageEvent(),
     ];
 
     if (0 != _imageFileList.length) {
       for (File file in _imageFileList) {
         list.insert(
-          0,
-          Container(
-            // red box
-            child: Platform.isAndroid
-                ? FutureBuilder<void>(
-                    future: retrieveLostData(),
-                    builder:
-                        (BuildContext context, AsyncSnapshot<void> snapshot) {
-                      switch (snapshot.connectionState) {
-                        case ConnectionState.none:
+            0,
+            GestureDetector(
+              onTap: () {
+                _previewImage();
+//                Navigator.pop(context);
+              },
+              child: Container(
+                // red box
+                child: Platform.isAndroid
+                    ? FutureBuilder<void>(
+                        future: retrieveLostData(),
+                        builder: (BuildContext context,
+                            AsyncSnapshot<void> snapshot) {
+                          switch (snapshot.connectionState) {
+                            case ConnectionState.none:
 
-                        case ConnectionState.waiting:
-                          return const Text(
-                            '2222.',
-                            textAlign: TextAlign.center,
-                          );
-                        case ConnectionState.done:
-                          return _buildImageWidget(file);
-                        default:
-                          if (snapshot.hasError) {
-                            return Text(
-                              'Pick image/video error: ${snapshot.error}}',
-                              textAlign: TextAlign.center,
-                            );
-                          } else {
-                            return const Text(
-                              '33 ',
-                              textAlign: TextAlign.center,
-                            );
+                            case ConnectionState.waiting:
+                              return const Text(
+                                '2222.',
+                                textAlign: TextAlign.center,
+                              );
+                            case ConnectionState.done:
+                              return _buildImageWidget(file);
+                            default:
+                              if (snapshot.hasError) {
+                                return Text(
+                                  'Pick image/video error: ${snapshot.error}}',
+                                  textAlign: TextAlign.center,
+                                );
+                              } else {
+                                return const Text(
+                                  '33 ',
+                                  textAlign: TextAlign.center,
+                                );
+                              }
                           }
-                      }
-                    },
-                  )
-                : _buildImageWidget(file),
-            decoration: BoxDecoration(
+                        },
+                      )
+                    : _buildImageWidget(file),
+                decoration: BoxDecoration(
 //                    color: Colors.grey[300],
-                ),
-            width: 100,
-            height: 120,
-          ),
-        );
+                    ),
+                width: 100,
+                height: 120,
+              ),
+            ));
       }
     }
     return list;
+  }
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    await for (FileSystemEntity entity in directory.list()) {
+      //文件、目录和链接都继承自FileSystemEntity
+      //FileSystemEntity.type静态函数返回值为FileSystemEntityType
+      //FileSystemEntityType有三个常量：
+      //Directory、FILE、LINK、NOT_FOUND
+      //FileSystemEntity.isFile .isLink .isDerectory可用于判断类型
+      print(entity.path);
+    }
+    return directory.path;
   }
 
   @override
@@ -221,7 +269,18 @@ class _WriteState extends State<WriteView> {
 //    print('isFuchsia${Platform.isFuchsia}');
     return Scaffold(
       appBar: AppBar(
-        title: Text("Sign in"),
+        // title: Text("Sign in"),
+        actions: <Widget>[
+          new IconButton(
+            // action button
+            icon: new Icon(Icons.save),
+            onPressed: () {
+              _localPath
+                  .then((onValue) => print(onValue))
+                  .catchError((onError) => print(onError));
+            },
+          ),
+        ],
       ),
       body: Center(
         child: Form(
