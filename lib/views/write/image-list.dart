aed3baa336d36e00ea2e0a6574022dd6c961ec16
liang <402284347@qq.com>
2019-12-13 22:44:35 +0800

    拆分组件

--- lib/views/write/image-list.dart
+++ lib/views/write/image-list.dart
@@ -1,44 +1,9 @@
-import 'package:image_picker/image_picker.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
-
-import 'dart:async';
 import 'dart:io';
 
-import './custom-Pagination-builder.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-void _previewImage(BuildContext context, List<File> _imageFileList) async {
-  await showDialog(
-    context: context,
-    builder: (ctx) {
-      //https://juejin.im/post/5c3f3c29f265da6120621048 说明Swiper
-      return Scaffold(
-        appBar: AppBar(
-            // title: Text("Sign in"),
-            // actions: <Widget>[
-            //   new IconButton(
-            //     // action button
-            //     icon: new Icon(Icons.save),
-            //     onPressed: () {
-            //       _localPath
-            //           .then((onValue) => print(onValue))
-            //           .catchError((onError) => print(onError));
-            //     },
-            //   ),
-            // ],
-            ),
-        body: Swiper(
-          itemBuilder: (BuildContext context, int index) =>
-              Image.file(_imageFileList[index]),
-          itemCount: _imageFileList.length,
-          index: 0,
-          pagination: new CustomPaginationBuilder(),
-          onTap: (index) => print('点击了第$index'),
-        ),
-      );
-    },
-  );
-}
+import 'image-item.dart';
 
 class ImageList extends StatefulWidget {
   @override
@@ -48,32 +13,6 @@ class ImageList extends StatefulWidget {
 class _ImageListState extends State<ImageList> {
   List<File> _imageFileList = [];
   dynamic _pickImageError;
-  String _retrieveDataError;
-
-  Text _getRetrieveErrorWidget() {
-    if (_retrieveDataError != null) {
-      final Text result = Text(_retrieveDataError);
-      _retrieveDataError = null;
-      return result;
-    }
-    return null;
-  }
-
-  Future<void> retrieveLostData() async {
-    final LostDataResponse response = await ImagePicker.retrieveLostData();
-    if (response.isEmpty) {
-      return;
-    }
-    if (response.file != null) {
-      setState(() {
-        File imageFile = response.file;
-        _imageFileList.add(imageFile);
-      });
-    } else {
-      _retrieveDataError = response.exception.code;
-    }
-  }
-
   void _onImageButtonPressed(ImageSource source) async {
     try {
       File imageFile = await ImagePicker.pickImage(source: source);
@@ -179,72 +118,6 @@ class _ImageListState extends State<ImageList> {
     );
   }
 
-  Widget _buildImageWidget(data) {
-    final Text retrieveError = _getRetrieveErrorWidget();
-    if (retrieveError != null) {
-      return retrieveError;
-    }
-    if (data != null) {
-      return Image.file(data);
-    } else if (_pickImageError != null) {
-      return Text(
-        'Pick image error: $_pickImageError',
-        textAlign: TextAlign.center,
-      );
-    } else {
-      return const Text(
-        '1111.',
-        textAlign: TextAlign.center,
-      );
-    }
-  }
-
-  Widget addImage(File file) {
-    return GestureDetector(
-      onTap: () {
-        _previewImage(context, _imageFileList);
-//              Navigator.pushNamed(context, '/login');
-//                Navigator.pop(context);
-      },
-      child: Container(
-        child: Platform.isAndroid
-            ? FutureBuilder<void>(
-                future: retrieveLostData(),
-                builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
-                  switch (snapshot.connectionState) {
-                    case ConnectionState.none:
-                    case ConnectionState.waiting:
-                      return const Text(
-                        '2222.',
-                        textAlign: TextAlign.center,
-                      );
-                    case ConnectionState.done:
-                      return _buildImageWidget(file);
-                    default:
-                      if (snapshot.hasError) {
-                        return Text(
-                          'Pick image/video error: ${snapshot.error}}',
-                          textAlign: TextAlign.center,
-                        );
-                      } else {
-                        return const Text(
-                          '33 ',
-                          textAlign: TextAlign.center,
-                        );
-                      }
-                  }
-                },
-              )
-            : _buildImageWidget(file),
-        decoration: BoxDecoration(
-//                    color: Colors.grey[300],
-            ),
-        width: 100,
-        height: 120,
-      ),
-    );
-  }
-
   List<Widget> _buildList(BuildContext context) {
     var list = <Widget>[
       _buildAddImageEvent(),
@@ -252,7 +125,7 @@ class _ImageListState extends State<ImageList> {
 
     if (0 != _imageFileList.length) {
       for (File file in _imageFileList) {
-        list.insert(0, addImage(file));
+        list.insert(0, ImageItem(_imageFileList, file));
       }
     }
     return list;
