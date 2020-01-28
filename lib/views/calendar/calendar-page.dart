a6396d1e9b4fdd1c5789407f0cccb7fecd93d93f
liang <qq402284347@gmail.com>
2020-01-28 21:44:19 +0800

    日历用例注解

--- lib/views/calendar/calendar-page.dart
+++ lib/views/calendar/calendar-page.dart
@@ -3,11 +3,12 @@ import 'package:table_calendar/table_calendar.dart';
 
 // Example holidays
 final Map<DateTime, List> _holidays = {
-  DateTime(2019, 1, 1): ['New Year\'s Day'],
-  DateTime(2019, 1, 6): ['Epiphany'],
-  DateTime(2019, 2, 14): ['Valentine\'s Day'],
-  DateTime(2019, 4, 21): ['Easter Sunday'],
-  DateTime(2019, 4, 22): ['Easter Monday'],
+  DateTime(2020, 1, 1): ['New Year\'s Day'],
+  DateTime(2020, 1, 6): ['Epiphany'],
+  DateTime(2020, 1, 7): ['Epiphany'],
+  DateTime(2020, 2, 14): ['Valentine\'s Day'],
+  DateTime(2020, 4, 21): ['Easter Sunday'],
+  DateTime(2020, 4, 22): ['Easter Monday'],
 };
 
 class CalendarPage extends StatefulWidget {
@@ -28,7 +29,10 @@ class _CalendarStatePage extends State<CalendarPage>
   void initState() {
     super.initState();
     final _selectedDay = DateTime.now();
+    print(_selectedDay);
+    print(_selectedDay.subtract(Duration(days: 27)));
 
+    // 时间 : 行程 Map<DateTime, List> _events
     _events = {
       _selectedDay.subtract(Duration(days: 30)): [
         'Event A0',
@@ -36,11 +40,11 @@ class _CalendarStatePage extends State<CalendarPage>
         'Event C0'
       ],
       _selectedDay.subtract(Duration(days: 27)): ['Event A1'],
-      _selectedDay.subtract(Duration(days: 20)): [
-        'Event A2',
-        'Event B2',
-        'Event C2',
-        'Event D2'
+      _selectedDay.subtract(Duration(days: 21)): [
+        'Event ?????A2',
+        'Event???? B2',
+        'Event?????? C2',
+        'Event??? D2'
       ],
       _selectedDay.subtract(Duration(days: 16)): ['Event A3', 'Event B3'],
       _selectedDay.subtract(Duration(days: 10)): [
@@ -54,9 +58,14 @@ class _CalendarStatePage extends State<CalendarPage>
         'Event C5'
       ],
       _selectedDay.subtract(Duration(days: 2)): ['Event A6', 'Event B6'],
-      _selectedDay: ['Event A7', 'Event B7', 'Event C7', 'Event D7'],
+      _selectedDay: [
+        'Event A7222',
+        '111Event B7',
+        '222Event C7',
+        '333Event D7'
+      ],
       _selectedDay.add(Duration(days: 1)): [
-        'Event A8',
+        '距离1',
         'Event B8',
         'Event C8',
         'Event D8'
@@ -110,19 +119,19 @@ class _CalendarStatePage extends State<CalendarPage>
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('77777777777777'),
+          title: Text('calendar'),
         ),
         body: Column(
           mainAxisSize: MainAxisSize.max,
           children: <Widget>[
             // Switch out 2 lines below to play with TableCalendar's settings
             //-----------------------
-            _buildTableCalendar(),
-            // _buildTableCalendarWithBuilders(),
-            const SizedBox(height: 8.0),
-            _buildButtons(),
-            const SizedBox(height: 8.0),
-            Expanded(child: _buildEventList()),
+            // _buildTableCalendar(), // 日历
+            _buildTableCalendarWithBuilders(),
+//            const SizedBox(height: 8.0),
+            // _buildButtons(),  // 日历控制器
+//            const SizedBox(height: 8.0),
+            Expanded(child: _buildEventList()), // 事务列表
           ],
         ),
       ),
@@ -130,7 +139,7 @@ class _CalendarStatePage extends State<CalendarPage>
   }
 
   void _onDaySelected(DateTime day, List events) {
-    print('CALLBACK: _onDaySelected');
+    print('CALLBACK: _onDaySelected??1111');
     setState(() {
       _selectedEvents = events;
     });
@@ -138,21 +147,22 @@ class _CalendarStatePage extends State<CalendarPage>
 
   void _onVisibleDaysChanged(
       DateTime first, DateTime last, CalendarFormat format) {
-    print('CALLBACK: _onVisibleDaysChanged');
+    print('CALLBACK: _onVisibleDaysChanged??2222');
   }
 
   // Simple TableCalendar configuration (using Styles)
   Widget _buildTableCalendar() {
     return TableCalendar(
       calendarController: _calendarController,
-      events: _events,
-      holidays: _holidays,
-      startingDayOfWeek: StartingDayOfWeek.monday,
+      events: _events, // 行程表
+      holidays: _holidays, // 节假日
+      startingDayOfWeek: StartingDayOfWeek.monday, // 一周的起始时间
       calendarStyle: CalendarStyle(
-        selectedColor: Colors.deepOrange[400],
-        todayColor: Colors.deepOrange[200],
-        markersColor: Colors.brown[700],
-        outsideDaysVisible: false,
+        // 日历日期样式
+        selectedColor: Colors.deepOrange[400], // 所选日期的背景色
+        todayColor: Colors.deepOrange[200], // 今天的背景色
+        markersColor: Colors.brown[700], // 行程颜色
+        outsideDaysVisible: true, // 是否补全日历
       ),
       headerStyle: HeaderStyle(
         formatButtonTextStyle:
@@ -162,46 +172,49 @@ class _CalendarStatePage extends State<CalendarPage>
           borderRadius: BorderRadius.circular(16.0),
         ),
       ),
-      onDaySelected: _onDaySelected,
-      onVisibleDaysChanged: _onVisibleDaysChanged,
+      onDaySelected: _onDaySelected, // 点击事件
+      onVisibleDaysChanged: _onVisibleDaysChanged, //日历翻页事件
     );
   }
 
   // More advanced TableCalendar configuration (using Builders & Styles)
   Widget _buildTableCalendarWithBuilders() {
     return TableCalendar(
-      locale: 'pl_PL',
+      // locale: 'pl_PL', // 语种
       calendarController: _calendarController,
-      events: _events,
-      holidays: _holidays,
+      events: _events, // 行程表
+      holidays: _holidays, // 节假日
       initialCalendarFormat: CalendarFormat.month,
       formatAnimation: FormatAnimation.slide,
-      startingDayOfWeek: StartingDayOfWeek.sunday,
+      startingDayOfWeek: StartingDayOfWeek.monday, // 一周的起始时间
       availableGestures: AvailableGestures.all,
       availableCalendarFormats: const {
         CalendarFormat.month: '',
         CalendarFormat.week: '',
       },
       calendarStyle: CalendarStyle(
-        outsideDaysVisible: false,
-        weekendStyle: TextStyle().copyWith(color: Colors.blue[800]),
-        holidayStyle: TextStyle().copyWith(color: Colors.blue[800]),
+        // 日历日期样式
+        outsideDaysVisible: true, // 是否补全日历
+        weekendStyle: TextStyle().copyWith(color: Colors.red), // 行程日样式
+        holidayStyle: TextStyle().copyWith(color: Colors.blue[800]), // 节假日样式
       ),
       daysOfWeekStyle: DaysOfWeekStyle(
-        weekendStyle: TextStyle().copyWith(color: Colors.blue[600]),
+        weekendStyle: TextStyle().copyWith(color: Colors.blue[600]), // 周末样式
       ),
       headerStyle: HeaderStyle(
-        centerHeaderTitle: true,
-        formatButtonVisible: false,
+        // 头部样式
+        centerHeaderTitle: true, // 头部标题是否居中
+        formatButtonVisible: false, // 格式化按钮是否显示
       ),
       builders: CalendarBuilders(
         selectedDayBuilder: (context, date, _) {
+          // 选中样式
           return FadeTransition(
             opacity: Tween(begin: 0.0, end: 1.0).animate(_animationController),
             child: Container(
               margin: const EdgeInsets.all(4.0),
               padding: const EdgeInsets.only(top: 5.0, left: 6.0),
-              color: Colors.deepOrange[300],
+              color: Colors.blue[400],
               width: 100,
               height: 100,
               child: Text(
@@ -212,10 +225,11 @@ class _CalendarStatePage extends State<CalendarPage>
           );
         },
         todayDayBuilder: (context, date, _) {
+          // 今日的样式
           return Container(
             margin: const EdgeInsets.all(4.0),
             padding: const EdgeInsets.only(top: 5.0, left: 6.0),
-            color: Colors.amber[400],
+            color: Colors.grey[400],
             width: 100,
             height: 100,
             child: Text(
@@ -251,10 +265,11 @@ class _CalendarStatePage extends State<CalendarPage>
         },
       ),
       onDaySelected: (date, events) {
+        // 点击事件
         _onDaySelected(date, events);
         _animationController.forward(from: 0.0);
       },
-      onVisibleDaysChanged: _onVisibleDaysChanged,
+      onVisibleDaysChanged: _onVisibleDaysChanged, //日历翻页事件
     );
   }
 
